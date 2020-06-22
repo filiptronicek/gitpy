a325374b84d3c84a97f0f1726b69ae727fef3a7f
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-22 14:58:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A Python wrapper for git",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/filiptronicek/gitpy",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
