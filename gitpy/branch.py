0a0ccecf3936e6686a3d9943bbb76a6f3d588505
Filip Troníček <filip.tronicek@seznam.cz>
2020-07-06 15:49:56 +0200

    Raise error if contains illegal chars

--- gitpy/branch.py
+++ gitpy/branch.py
@@ -1,6 +1,11 @@
 from os import system
 
+illegal = ["..","~","^",":","*","?", "\\"] # https://stackoverflow.com/a/3651867/10199319
+
 def createBranch(name, checkout=True):
+    for c in name:
+        if c in illegal:
+            raise ValueError(f"The character '{c}' cannot be part of a name of a branch")
     """ Description
         :param name: - name of branch to create
         :param checkout:
